6dc3039cf6339812cf133dd97422a34e236eff6b
rgenchev <rgenchev@hey.com>
2021-11-13 21:57:10 +0200

    LinkedListTest: test initialization

--- test/data_structures/linked_list/linked_list_test.rb
+++ test/data_structures/linked_list/linked_list_test.rb
@@ -1,3 +1,16 @@
 require "test_helper"
 
-class LinkedListTest < Minitest::Test; end
+class LinkedListTest < Minitest::Test
+  def setup
+    @linked_list = LinkedList.new
+  end
+
+  def test_should_initialize_with_head_and_tail_set_to_nil
+    assert_nil @linked_list.head
+    assert_nil @linked_list.tail
+  end
+
+  def test_should_initialize_with_length_set_to_zero
+    assert_equal 0, @linked_list.length
+  end
+end
