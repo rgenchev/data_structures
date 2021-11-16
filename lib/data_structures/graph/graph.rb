05aa2456288dbe2795f5067e5fa1525adbf79ef9
rgenchev <rgenchev@hey.com>
2021-11-16 15:23:58 +0200

    DataStructures::Graph::Graph: remove some 'puts' statements

--- lib/data_structures/graph/graph.rb
+++ lib/data_structures/graph/graph.rb
@@ -31,52 +31,9 @@ class Graph
     unless directed
       node_2.edges.push(node_1)
     end
-
-    puts "An edge between #{node_1.value} and #{node_2.value} was created."
   end
 
   def get_node(value)
     nodes.find { |node| node.value == value }
   end
-end
-
-puts "Creating the graph..."
-
-graph_bulgarian_cities = Graph.new
-
-puts "..."
-
-puts "Creating nodes..."
-
-puts "Adding 'Sofia' to the graph..."
-graph_bulgarian_cities.add_node("Sofia")
-
-puts "Adding 'Plovdiv' to the graph..."
-graph_bulgarian_cities.add_node("Plovdiv")
-
-puts "Adding 'Varna' to the graph..."
-graph_bulgarian_cities.add_node("Varna")
-
-puts "..."
-
-graph_bulgarian_cities.add_edge("Sofia", "Plovdiv")
-graph_bulgarian_cities.add_edge("Sofia", "Varna")
-graph_bulgarian_cities.add_edge("Plovdiv", "Varna")
-
-puts "..."
-puts "Cities in the graph: #{graph_bulgarian_cities.nodes.map(&:value)}"
-graph_bulgarian_cities.nodes.each do |node|
-  puts "#{node.value} node has the following edges: #{node.edges.map(&:value)}"
-end
-
-puts "..."
-
-puts "Removing 'Varna'..."
-graph_bulgarian_cities.remove_node("Varna")
-
-puts "..."
-
-puts "Cities in the graph: #{graph_bulgarian_cities.nodes.map(&:value)}"
-graph_bulgarian_cities.nodes.each do |node|
-  puts "#{node.value} node has the following edges: #{node.edges.map(&:value)}"
 end
