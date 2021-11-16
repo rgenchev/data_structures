13000b5a8a5878de44bff46d273482c043cd0537
rgenchev <rgenchev@hey.com>
2021-11-16 15:23:16 +0200

    DataStructures: require 'Graph' class

--- lib/data_structures.rb
+++ lib/data_structures.rb
@@ -1,6 +1,7 @@
 require "data_structures/version"
 
 require "data_structures/linked_list/linked_list"
+require "data_structures/graph/graph"
 
 module DataStructures
   class Error < StandardError; end
