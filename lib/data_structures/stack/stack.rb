ab6803f585faf76486667a809cbb74b63e635d95
Radoslav Genchev <rgenchev@hey.com>
2022-01-22 22:30:19 +0200

    Stack: do some refactoring and remove 'puts' statements

--- lib/data_structures/stack/stack.rb
+++ lib/data_structures/stack/stack.rb
@@ -6,44 +6,22 @@ class Stack
   end
 
   def push(item)
-    stack.push(item)
+    @stack.push(item)
   end
 
   def pop
-    stack.pop
+    @stack.pop
   end
 
   def peek
-    stack[length - 1]
+    @stack[length - 1]
   end
 
   def empty?
-    stack.length == 0
+    @stack.length == 0
   end
 
   def length
-    stack.length
+    @stack.length
   end
 end
-
-puts "Creating a stack"
-stack = Stack.new
-
-puts "Stack empty? - #{stack.empty?}"
-
-stack.push(1)
-stack.push(2)
-stack.push(3)
-
-puts "Stack: #{stack.stack}"
-puts "Stack length: #{stack.length}"
-
-puts "Popping from the stack"
-stack.pop
-
-puts "Stack: #{stack.stack}"
-puts "Stack length: #{stack.length}"
-
-puts "Peek: #{stack.peek}"
-
-puts "Stack empty? - #{stack.empty?}"
