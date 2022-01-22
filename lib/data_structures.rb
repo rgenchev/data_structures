056106c6def32c35d9de7993a4bc37e0107ef169
Radoslav Genchev <rgenchev@hey.com>
2022-01-22 22:30:37 +0200

    Require Stack

--- lib/data_structures.rb
+++ lib/data_structures.rb
@@ -3,6 +3,7 @@ require "data_structures/version"
 require "data_structures/linked_list/linked_list"
 require "data_structures/graph/graph"
 require "data_structures/queue/queue"
+require "data_structures/stack/stack"
 
 module DataStructures
   class Error < StandardError; end
