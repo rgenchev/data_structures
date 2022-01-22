adec71344054e09fec216e3e9f73a9cf9e6636cf
Radoslav Genchev <rgenchev@hey.com>
2022-01-22 22:15:39 +0200

    Queue::Queue: do some refactoring and remove puts statements

--- lib/data_structures/queue/queue.rb
+++ lib/data_structures/queue/queue.rb
@@ -1,56 +1,25 @@
-class Queue
-  attr_accessor :queue
-
+class Queue::Queue
   def initialize
     @queue = []
   end
 
   def enqueue(item)
-    queue.push(item)
+    @queue.push(item)
   end
 
   def dequeue
-    queue.shift
+    @queue.shift
   end
 
   def peek
-    queue[0]
+    @queue[0]
   end
 
   def empty?
-    queue.length == 0
+    @queue.length == 0
   end
 
   def length
-    queue.length
+    @queue.length
   end
 end
-
-puts "Creating a queue"
-queue = Queue.new
-
-puts "Queue empty? - #{queue.empty?}"
-
-puts "Enqueuing some items..."
-queue.enqueue(1)
-queue.enqueue(2)
-queue.enqueue(3)
-
-puts "Queue: #{queue.queue}"
-puts "Queue length: #{queue.length}"
-
-puts "Dequeuing..."
-queue.dequeue
-
-puts "Queue: #{queue.queue}"
-puts "Queue length: #{queue.length}"
-
-puts "Peek: #{queue.peek}"
-
-puts "Dequeuing..."
-queue.dequeue
-
-puts "Queue: #{queue.queue}"
-puts "Queue length: #{queue.length}"
-
-puts "Queue empty? - #{queue.empty?}"
